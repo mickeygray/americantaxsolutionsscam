1476e65d34891f7eb7c3175e1dcf165a646e17f6
mickeygray <mgray@nattaxexperts.com>
2020-07-09 08:39:02 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from "react";
+import {
+  useLocation,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from "./components/Home";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Switch>
+          <div>
+            <Route exact path='/' component={Home} />
+          </div>
+        </Switch>
+      </Fragment>
+    </Router>
+  );
+};
+
+export default App;
